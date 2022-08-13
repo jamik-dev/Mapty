30ad78750b0ebe3c94ba87258aae081c96c878e2
jamik-dev <jamik.oik.group@gmail.com>
2022-08-13 15:57:32 +0500

    setting map

--- script/script.js
+++ script/script.js
@@ -13,7 +13,17 @@ const inputElevation = document.querySelector('.form__input--elevation');
 if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(
     function(position) {
-      console.log(position);
+      const {latitude} = position.coords;
+      const {longitude} = position.coords;
+      const coords = [latitude, longitude];
+      const map = L.map('map').setView(coords, 13);
+      L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      }).addTo(map);
+
+      L.marker(coords).addTo(map)
+        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+        .openPopup();
     }, 
     function() {
       alert('Could not get current location');
